7f668ba217189d685dda568cd9cd67945aa44b0d
alb395 <alb395@nyu.edu>
2016-07-18 17:34:33 -0400

    Session 5 homework attempt
    Attempt to have a multi-question quiz for session 5.

--- session5/src/fibber.js
+++ session5/src/fibber.js
@@ -1,122 +1,65 @@
-/*
-SESSION 5 - Coding Basics
-
-1. Setting text of a div
-2. If/then logic
-3. Using the console
-4. Creating objects to store data
-5. Handling other events besides "click"
-
-*/
-
-
-//var is used to create variables
-//using the {} to create a variable that is type object
 
 var fibber = {};
 
 
-fibber.sayhi = function() {
-		alert("holy shit it works");
-	};
-
-
-//this is a function that can be called using fibber.init();
 fibber.initialize = function(){
-	fibber.sayhi();
+
+	$("#startbtn").click(function() {
+		fibber.start();
+	});
+
+	fibber.viewStatement();
+
+	//setup all the true/false buttons to call processDecision
+
+	$(".truth-btn").click(function(){
+		fibber.processDecision("truth");
+	});
+
+	$(".fib-btn").click(function(){
+		fibber.processDecision("fib");
+	});
+
+};
+
+//Create arrays that contain the statement-text and statement-context. I could probably do this with a two-dimensional array instead of two separate arrays, but I'm not sure how to loop through that yet. 
+
+var statementArray = ["I am in New York", "I am in Los Angeles", "I am in Berlin"];
+var contextArray = ["Lexie Bryan", "Jessica Plummer", "Erica DeSouza"];
+
+var i = 0
+var currentStatement = statementArray[i];
+var currentContext = contextArray[i];
+
+fibber.viewStatement = function(){
+	$("#statement-text").text(currentStatement);
+	$("#statement-context").text(currentContext);
+	i = i++;
 };
 
 
-function add(x,y){
+//One approach was to create a for loop that will progress through the array to display one questionText at a time. However, I wasn't sure how to get the index to only increment after the user clicked a truth or fib button. 
 
-	alert(x + y);
-};
-
-add(2,3);
-add(5,1);
-
-// fibber.start = function(){
-// 	alert("Start Button Clicked...");
+// fibber.viewStatement = function(){
+// 	for (var i = 0; i < statementArray.length; i++) {
+// 		$("#statement-text").text(statementArray[i]);
+// 		$("#statement-context").text(contextArray[i]);
+// 	}
+	
 // };
 
 
-
-
-
-
-
-
-
-
-
-
-/*
-
----------------------------------IGNORE ALL OF THIS-------------------
-
-fibber.viewStatement = function(){
-};
-
-fibber.processDecision = function(decision){
-}
-
-fibber.log = function(msg){
-	console.log(msg);
-};
-
-fibber.loadRemoteContent = function(callback){
-	
-};
-
-
-
-fibber.loadViewStatementPanel = function(){
-	fibber.log("view: loadViewStatementPanel() called..");
-
-	//setup the fib and truth buttons
-	$(".fib-btn").off().on("tap", function(){
-		fibber.processDecision('fib');
-	});
-	$(".truth-btn").off().on("tap", function(){
-		fibber.processDecision('truth');
-	});
-
-	//display the current statement
+fibber.processDecision = function(decision) {
+	if (decision == "truth") {
+		alert("That's true");
+	}
+	if (decision == "fib") {
+		alert("That was a fib");
+	}
 	fibber.viewStatement();
 };
 
-fibber.loadInstructionsPanel = function(){
-	fibber.log("view: loadInstructionsPanel() called..");
 
-	//assume player is restarting the session
-	//1. clear all variables holding session data
-	fibber.clintonCounter = 0;
-	fibber.trumpCounter = 0;
-	fibber.activeCharacter = "";
-	fibber.clintonLives = 5;
-	fibber.trumpLives = 5;
-	fibber.playerLives = 5;
-
-	//2. reset any visuals that were moved/modified
+fibber.start = function(){
+	alert("Start Button Clicked...");
 };
-
-
-fibber.viewFeedback = function(title, msg){
-
-	$.afui.popup( {
-	   title:title,
-	   message:msg,
-	   cancelText:"CONTINUE",
-	   cancelCallback: function(){
-	   	fibber.viewStatement();
-	   },
-	   cancelOnly:true
-	 });
-
-};
-
-//setup custom on screen load functionality
-	$("#instructions").on("panelload", function(){fibber.loadInstructionsPanel();});
-	$("#viewstatement").on("panelload", function(){fibber.loadViewStatementPanel();});
-
-*/
